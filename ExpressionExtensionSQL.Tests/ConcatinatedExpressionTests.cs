6ebc857b53dad8af9f5a4884a279cb013606baa0
Boristy <czou1993@163.com>
2024-06-28 16:19:02 +0800

    1. support Tirm()、HasValue、Nullable.Value 2. change single true\false

--- ExpressionExtensionSQL.Tests/ConcatinatedExpressionTests.cs
+++ ExpressionExtensionSQL.Tests/ConcatinatedExpressionTests.cs
@@ -14,7 +14,7 @@ namespace ExpressionExtensionSQL.Tests
         {
             Expression<Func<Merchant, bool>> expression = x => false || x.Name == "merchant1";
             var where = expression.ToSql();
-            where.Sql.Should().Be("(0=1 OR ([Merchant].[Name] = @1))");
+            where.Sql.Should().Be("(0=1 OR ([Merchant].[Name] = @p1))");
         }
 
         [Fact(DisplayName = "ConcatenatedExpression - true OR Equal")]
@@ -22,7 +22,7 @@ namespace ExpressionExtensionSQL.Tests
         {
             Expression<Func<Merchant, bool>> expression = x => true || x.Name == "merchant1";
             var where = expression.ToSql();
-            where.Sql.Should().Be("(1=1 OR ([Merchant].[Name] = @1))");
+            where.Sql.Should().Be("(1=1 OR ([Merchant].[Name] = @p1))");
         }
 
         [Fact(DisplayName = "ConcatenatedExpression - false AND Equal")]
@@ -30,7 +30,7 @@ namespace ExpressionExtensionSQL.Tests
         {
             Expression<Func<Merchant, bool>> expression = x => false && x.Name == "merchant1";
             var where = expression.ToSql();
-            where.Sql.Should().Be("(0=1 AND ([Merchant].[Name] = @1))");
+            where.Sql.Should().Be("(0=1 AND ([Merchant].[Name] = @p1))");
         }
 
         [Fact(DisplayName = "ConcatenatedExpression - true AND Equal")]
@@ -38,7 +38,15 @@ namespace ExpressionExtensionSQL.Tests
         {
             Expression<Func<Merchant, bool>> expression = x => true && x.Name == "merchant1";
             var where = expression.ToSql();
-            where.Sql.Should().Be("(1=1 AND ([Merchant].[Name] = @1))");
+            where.Sql.Should().Be("(1=1 AND ([Merchant].[Name] = @p1))");
+        }
+
+        [Fact(DisplayName = "ConcatenatedExpression - NOT HasValue AND Equal")]
+        public void HasValueAndEqualExpression()
+        {
+            Expression<Func<Merchant, bool>> expression = x => !x.Status.HasValue && x.Name == "merchant1";
+            var where = expression.ToSql();
+            where.Sql.Should().Be("(([Merchant].[Status] IS NULL) AND ([Merchant].[Name] = @p1))");
         }
     }
 }
